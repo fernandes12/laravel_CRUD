3d0a80d73d299af6da22363d147a7605a49f1784
fernandes12 <fernandesjhd@gmail.com>
2020-06-24 17:28:00 +0100

    Read from database!

--- app/Models/ModelBook.php
+++ app/Models/ModelBook.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ModelBook extends Model
 {
     protected $table = 'book';
+
+    public function relUsers(){
+        //One book can only have one user (author)
+        return $this->hasOne('App\User', 'id', 'id_user'); //Class , foreign key, local key
+    }
 }
