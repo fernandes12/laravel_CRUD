fe5f8b7b43aa526d7431030f6dc6da9d31b453a7
fernandes12 <fernandesjhd@gmail.com>
2020-06-25 15:14:06 +0100

    Some changes

--- app/Models/ModelBook.php
+++ app/Models/ModelBook.php
@@ -9,6 +9,7 @@ class ModelBook extends Model
     protected $table = 'book';
 
     public function relUsers(){
+        
         //One book can only have one user (author)
         return $this->hasOne('App\User', 'id', 'id_user'); //Class , foreign key, local key
     }
